8a28e43d46f66d4bbabad6d5d45aa82aca7bced9
Saharsh Bhatia <friedchickenfordinner@gmail.com>
2020-05-18 01:57:08 +1000

    Update frontend to fetch via slug

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -19,11 +19,11 @@
           <b-nav-item :to="'/'">Home</b-nav-item>
           <!-- <b-nav-item :to="'/'">Help Homeless</b-nav-item> -->
           <b-nav-item-dropdown text="Seek Help">
-            <b-dropdown-item :to="'/seek-help/1'">Essential Services</b-dropdown-item>
-            <b-dropdown-item :to="'/seek-help/2'">Elder Care</b-dropdown-item>
-            <b-dropdown-item :to="'/seek-help/3'">Get healthy</b-dropdown-item>
-            <b-dropdown-item :to="'/seek-help/4'">Help from addiction</b-dropdown-item>
-            <b-dropdown-item :to="'/seek-help/5'">Community Services</b-dropdown-item>
+            <b-dropdown-item :to="'/seek-help/essential'">Essential Services</b-dropdown-item>
+            <b-dropdown-item :to="'/seek-help/eldercare'">Elder Care</b-dropdown-item>
+            <b-dropdown-item :to="'/seek-help/healthy'">Get healthy</b-dropdown-item>
+            <b-dropdown-item :to="'/seek-help/addiction'">Help from addiction</b-dropdown-item>
+            <b-dropdown-item :to="'/seek-help/community'">Community Services</b-dropdown-item>
           </b-nav-item-dropdown>
           <!-- <b-nav-item :to="'/'">Contact us</b-nav-item> -->
         </b-navbar-nav>
--- src/views/Homepage.v2.vue
+++ src/views/Homepage.v2.vue
@@ -70,11 +70,11 @@
             </b-row>
             <b-row align-h="center">
               <div class="flex-wrap" style="justify-content:center;" id="tiles-container">
-                <TopicTile class="mx-3 my-1" :slug="'1'" :title="'Essential Services'" :iconImage="'seekhelp_essential_icon'" :tileBackground="'seekhelp_essential_bg'"/>
-                <TopicTile class="mx-3 my-1" :slug="'2'" :title="'Elder Care'" :iconImage="'seekhelp_feet_icon'" :tileBackground="'seekhelp_feet_bg'"/>
-                <TopicTile class="mx-3 my-1" :slug="'3'" :title="'Get Healthy'" :iconImage="'seekhelp_health_icon'" :tileBackground="'seekhelp_health_bg'"/>
-                <TopicTile class="mx-3 my-1" :slug="'4'" :title="'Help from Addiction'" :iconImage="'seekhelp_addiction_icon'" :tileBackground="'seekhelp_addiction_bg'"/>
-                <TopicTile class="mx-3 my-1" :slug="'5'" :title="'Comminity Services'" :iconImage="'seekhelp_community_icon'" :tileBackground="'seekhelp_community_bg'"/>
+                <TopicTile class="mx-3 my-1" :slug="'essential'" :title="'Essential Services'" :iconImage="'seekhelp_essential_icon'" :tileBackground="'seekhelp_essential_bg'"/>
+                <TopicTile class="mx-3 my-1" :slug="'eldercare'" :title="'Elder Care'" :iconImage="'seekhelp_feet_icon'" :tileBackground="'seekhelp_feet_bg'"/>
+                <TopicTile class="mx-3 my-1" :slug="'healthy'" :title="'Get Healthy'" :iconImage="'seekhelp_health_icon'" :tileBackground="'seekhelp_health_bg'"/>
+                <TopicTile class="mx-3 my-1" :slug="'addiction'" :title="'Help from Addiction'" :iconImage="'seekhelp_addiction_icon'" :tileBackground="'seekhelp_addiction_bg'"/>
+                <TopicTile class="mx-3 my-1" :slug="'community'" :title="'Comminity Services'" :iconImage="'seekhelp_community_icon'" :tileBackground="'seekhelp_community_bg'"/>
               </div>
             </b-row>
           </b-container>
